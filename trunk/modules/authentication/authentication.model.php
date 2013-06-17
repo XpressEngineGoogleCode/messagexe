0a0dfc97a1336fab367ee23da90776537bcb7749
yubin@nurigo.net <yubin@nurigo.net@8ca1c35f-e68c-b9dd-db60-6d8c1d10ebfd>
2013-06-17 08:52:24 +0000

    중복방지추가

--- trunk/modules/authentication/authentication.model.php
+++ trunk/modules/authentication/authentication.model.php
@@ -25,6 +25,7 @@ class authenticationModel extends authentication
 			if(!$config->resend_interval) $config->resend_interval = 20;
 			if(!$config->day_try_limit) $config->day_try_limit = 10;
 			if(!$config->message_content) $config->message_content = '[핸드폰인증] %authcode% ☜  인증번호를 정확히 입력해 주세요';
+			if(!$config->number_overlap) $config->number_overlap = 'Y';
 			$GLOBALS['__authentication_config__'] = $config;
 		}
 		return $GLOBALS['__authentication_config__'];
