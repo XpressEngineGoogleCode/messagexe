8a1a63864106d43d796c1a2d6887c0f253ae59be
billy <billy@nurigo.net>
2014-06-10 06:11:18 +0000

    textmessage 코드 정리 및 불필요 코드 삭제

--- modules/textmessage/textmessage.class.php
+++ modules/textmessage/textmessage.class.php
@@ -1,40 +1,42 @@
 <?php
+/**
+ * vi:set sw=4 ts=4 noexpandtab fileencoding=utf8:
+ * @class  textmessage
+ * @author wiley (wiley@nurigo.net)
+ * @brief  base class of textmessage module
+ **/
+
+class textmessage extends ModuleObject {
+
 	/**
-	 * @class  textmessage
-	 * @author wiley (wiley@nurigo.net)
-	 * @brief  base class of textmessage module
+	 * @brief install textmessage module
+	 * @return new Object
 	 **/
-
-	class textmessage extends ModuleObject {
-
-		/**
-		 * @brief install textmessage module
-		 * @return new Object
-		 **/
-		function moduleInstall() {
-			return new Object();
-		}
-
-		/**
-		 * @brief if update is necessary it returns true
-		 **/
-		function checkUpdate() {
-			return false;
-		}
-
-		/**
-		 * @brief update module
-		 * @return new Object
-		 **/
-		function moduleUpdate() {
-			return new Object();
-		}
-
-		/**
-		 * @brief regenerate cache file
-		 * @return none
-		 **/
-		function recompileCache() {
-		}
+	function moduleInstall() {
+		return new Object();
 	}
-?>
+
+	/**
+	 * @brief if update is necessary it returns true
+	 **/
+	function checkUpdate() {
+		return false;
+	}
+
+	/**
+	 * @brief update module
+	 * @return new Object
+	 **/
+	function moduleUpdate() {
+		return new Object();
+	}
+
+	/**
+	 * @brief regenerate cache file
+	 * @return none
+	 **/
+	function recompileCache() {
+	}
+}
+/* End of file textmessage.class.php */
+/* Location: ./modules/textmessage.class.php */
