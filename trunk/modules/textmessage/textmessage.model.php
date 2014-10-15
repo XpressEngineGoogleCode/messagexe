d9dc1bdd36b07cb4c22cbaef92a8cde6184c4fe8
wiley@nurigo.net <wiley@nurigo.net@8ca1c35f-e68c-b9dd-db60-6d8c1d10ebfd>
2014-10-15 06:10:28 +0000

    deferred_payment 추가

--- trunk/modules/textmessage/textmessage.model.php
+++ trunk/modules/textmessage/textmessage.model.php
@@ -188,6 +188,7 @@ class textmessageModel extends textmessage
 		$obj = new Object();
 		$obj->add('cash', $result->cash);
 		$obj->add('point', $result->point);
+		$obj->add('deferred_payment', $result->deferred_payment);
 		$obj->add('sms_price', '20');
 		$obj->add('lms_price', '50');
 		$obj->add('mms_price', '200');
